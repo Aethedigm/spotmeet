b75b041f5dfafdc86579d99379e3a12f5fa6b51e
Stephen <aethedigm@gmail.com>
2022-10-22 16:27:12 -0400

    Update routes.go
    Converting to proper go-chi routing

--- myapp/routes.go
+++ myapp/routes.go
@@ -5,12 +5,18 @@ import (
 	"myapp/data"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	_ "github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
 )
 
 func (a *application) routes() *chi.Mux {
 	// middleware must come before any routes
+	a.App.Routes.Use(render.SetContentType(render.ContentTypeJSON))
+	a.App.Routes.Use(middleware.Timeout(60 * time.Second))
 
 	// add routes here
 	a.App.Routes.Get("/", a.Handlers.Home)
@@ -18,9 +24,13 @@ func (a *application) routes() *chi.Mux {
 	a.App.Routes.Get("/jet-page", a.Handlers.JetPage)
 	a.App.Routes.Get("/sessions", a.Handlers.SessionTest)
 
-	a.App.Routes.Get("/users/login", a.Handlers.UserLogin)
-	a.App.Routes.Post("/users/login", a.Handlers.PostUserLogin)
-	a.App.Routes.Get("/users/logout", a.Handlers.Logout)
+	a.App.Routes.Route("/users", func(r chi.Router) {
+		r.Get("/login", a.Handlers.UserLogin)
+		r.Post("/login", a.Handlers.PostUserLogin)
+		r.Get("/logout", a.Handlers.Logout)
+
+		r.Post("/create", a.Handlers.CreateUser)
+	})
 
 	a.App.Routes.Get("/create-user", func(w http.ResponseWriter, r *http.Request) {
 		u := data.User{
@@ -87,4 +97,12 @@ func (a *application) routes() *chi.Mux {
 	a.App.Routes.Handle("/public/*", http.StripPrefix("/public", fileServer))
 
 	return a.App.Routes
+}
+
+type ErrResponse struct {
+	Err            error  `json:"-"`
+	HTTPStatusCode int    `json:"-"`
+	StatusText     string `json:"status"`
+	AppCode        int64  `json:"code,omitempty"`
+	ErrorText      string `json:"error,omitempty"`
 }
