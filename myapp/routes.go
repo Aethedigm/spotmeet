2fb273bf5f5692667cdfcc13cca8343c81367059
Stephen <aethedigm@gmail.com>
2022-10-22 16:44:29 -0400

    Strip generic routes

--- myapp/routes.go
+++ myapp/routes.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"myapp/data"
 	"net/http"
-	"strconv"
 	"time"
 
 	"github.com/go-chi/chi/v5"
@@ -20,9 +17,6 @@ func (a *application) routes() *chi.Mux {
 
 	// add routes here
 	a.App.Routes.Get("/", a.Handlers.Home)
-	a.App.Routes.Get("/go-page", a.Handlers.GoPage)
-	a.App.Routes.Get("/jet-page", a.Handlers.JetPage)
-	a.App.Routes.Get("/sessions", a.Handlers.SessionTest)
 
 	a.App.Routes.Route("/users", func(r chi.Router) {
 		r.Get("/login", a.Handlers.UserLogin)
@@ -32,65 +26,36 @@ func (a *application) routes() *chi.Mux {
 		r.Post("/create", a.Handlers.CreateUser)
 	})
 
-	a.App.Routes.Get("/create-user", func(w http.ResponseWriter, r *http.Request) {
-		u := data.User{
-			FirstName: "Trevor",
-			LastName:  "Sawler",
-			Email:     "me@here.com",
-			Active:    1,
-			Password:  "password",
-		}
+	// a.App.Routes.Get("/get-user/{id}", func(w http.ResponseWriter, r *http.Request) {
+	// 	id, _ := strconv.Atoi(chi.URLParam(r, "id"))
 
-		id, err := a.Models.Users.Insert(u)
-		if err != nil {
-			a.App.ErrorLog.Println(err)
-			return
-		}
+	// 	u, err := a.Models.Users.Get(id)
+	// 	if err != nil {
+	// 		a.App.ErrorLog.Println(err)
+	// 		return
+	// 	}
 
-		fmt.Fprintf(w, "%d: %s", id, u.FirstName)
-	})
+	// 	fmt.Fprintf(w, "%s %s %s", u.FirstName, u.LastName, u.Email)
+	// })
 
-	a.App.Routes.Get("/get-all-users", func(w http.ResponseWriter, r *http.Request) {
-		users, err := a.Models.Users.GetAll()
-		if err != nil {
-			a.App.ErrorLog.Println(err)
-			return
-		}
-		for _, x := range users {
-			fmt.Fprint(w, x.LastName)
-		}
-	})
+	// a.App.Routes.Get("/update-user/{id}", func(w http.ResponseWriter, r *http.Request) {
+	// 	id, _ := strconv.Atoi(chi.URLParam(r, "id"))
+	// 	u, err := a.Models.Users.Get(id)
+	// 	if err != nil {
+	// 		a.App.ErrorLog.Println(err)
+	// 		return
+	// 	}
 
-	a.App.Routes.Get("/get-user/{id}", func(w http.ResponseWriter, r *http.Request) {
-		id, _ := strconv.Atoi(chi.URLParam(r, "id"))
+	// 	u.LastName = a.App.RandomString(10)
+	// 	err = u.Update(*u)
+	// 	if err != nil {
+	// 		a.App.ErrorLog.Println(err)
+	// 		return
+	// 	}
 
-		u, err := a.Models.Users.Get(id)
-		if err != nil {
-			a.App.ErrorLog.Println(err)
-			return
-		}
+	// 	fmt.Fprintf(w, "updated last name to %s", u.LastName)
 
-		fmt.Fprintf(w, "%s %s %s", u.FirstName, u.LastName, u.Email)
-	})
-
-	a.App.Routes.Get("/update-user/{id}", func(w http.ResponseWriter, r *http.Request) {
-		id, _ := strconv.Atoi(chi.URLParam(r, "id"))
-		u, err := a.Models.Users.Get(id)
-		if err != nil {
-			a.App.ErrorLog.Println(err)
-			return
-		}
-
-		u.LastName = a.App.RandomString(10)
-		err = u.Update(*u)
-		if err != nil {
-			a.App.ErrorLog.Println(err)
-			return
-		}
-
-		fmt.Fprintf(w, "updated last name to %s", u.LastName)
-
-	})
+	// })
 
 	// static routes
 	fileServer := http.FileServer(http.Dir("./public"))
