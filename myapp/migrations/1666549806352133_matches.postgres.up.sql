d81f5b1329f0d7246071411bcaa140c8f52514a7
Stephen <aethedigm@gmail.com>
2022-10-23 14:58:51 -0400

    Generated Match migrations

---
+++ myapp/migrations/1666549806352133_matches.postgres.up.sql
@@ -0,0 +1,10 @@
+drop table if exists matches cascade;
+
+CREATE TABLE matches (
+    id SERIAL PRIMARY KEY,
+    user_a_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    user_b_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    percent_match float NOT NULL,
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
+    expiry timestamp without time zone NOT NULL
+);
