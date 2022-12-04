07080e5cfcd19c9e13deae13824e4045ec9110ee
Stephen <aethedigm@gmail.com>
2022-12-04 03:07:51 -0500

    Clean up artist

--- myapp/data/artist.go
+++ myapp/data/artist.go
@@ -30,22 +30,6 @@ func (a *Artist) GetOneID() (int, error) {
 	return art.ID, nil
 }
 
-// GetAll returns a slice of all artists
-func (a *Artist) GetAll() ([]*Artist, error) {
-	collection := upper.Collection(a.Table())
-
-	var all []*Artist
-
-	res := collection.Find().OrderBy("artist_name")
-	err := res.All(&all)
-	if err != nil {
-		return nil, err
-	}
-
-	return all, nil
-}
-
-// GetByName gets one artist, by name
 func (a *Artist) GetByName(name string) (*Artist, error) {
 	var theArtist Artist
 	collection := upper.Collection(a.Table())
@@ -56,53 +40,6 @@ func (a *Artist) GetByName(name string) (*Artist, error) {
 	}
 
 	return &theArtist, nil
-}
-
-// Get gets one artist by id
-func (a *Artist) Get(id int) (*Artist, error) {
-	var theArtist Artist
-	collection := upper.Collection(a.Table())
-	res := collection.Find(up.Cond{"id": id})
-
-	err := res.One(&theArtist)
-	if err != nil {
-		return nil, err
-	}
-
-	return &theArtist, nil
-}
-
-// Update updates an artist record in the database
-func (a *Artist) Update(theArtist Artist) error {
-	collection := upper.Collection(a.Table())
-	res := collection.Find(theArtist.ID)
-	err := res.Update(&theArtist)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-// Delete deletes an artist record by id
-func (a *Artist) Delete(id int) error {
-	collection := upper.Collection(a.Table())
-	res := collection.Find(id)
-	err := res.Delete()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-// DeleteByName deletes an artist record by name
-func (a *Artist) DeleteByName(artistName string) error {
-	collection := upper.Collection(a.Table())
-	res := collection.Find(up.Cond{"artist_name": artistName})
-	err := res.Delete()
-	if err != nil {
-		return err
-	}
-	return nil
 }
 
 // Insert inserts a new artist, and returns the newly inserted id
